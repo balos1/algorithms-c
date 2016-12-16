09cbb6595d263ea3ecabeabaf5e5cd71f3e6532e
Cody Balos <cjbalos@gmail.com>
2016-12-15 23:49:52 -0800

    Extended tests for ringbuffer.

--- ringbuffer/tests.c
+++ ringbuffer/tests.c
@@ -13,7 +13,7 @@ void ringbuffer_print(ringbuffer* rbuf)
 
 int main(void)
 {
-	ringbuffer* rbuffer = new_ringbuffer(4);
+	ringbuffer* rbuffer = new_ringbuffer(8);
 
 	if (rbuffer == NULL)
 	{
@@ -26,7 +26,6 @@ int main(void)
 	ringbuffer_print(rbuffer);
 	printf("tail=%d\n", rbuffer->tail);
 	printf("head=%d\n", rbuffer->head);
-	// printf("count=%d\n", rbuffer->count);
 	
 
 	ringbuffer_insert(rbuffer, 'o');
@@ -34,62 +33,92 @@ int main(void)
 	ringbuffer_print(rbuffer);
 	printf("tail=%d\n", rbuffer->tail);
 	printf("head=%d\n", rbuffer->head);
-	// printf("count=%d\n", rbuffer->count);
 
 	ringbuffer_insert(rbuffer, 'd');
 	printf("\ninserted %c\n", 'd');
 	ringbuffer_print(rbuffer);
 	printf("tail=%d\n", rbuffer->tail);
 	printf("head=%d\n", rbuffer->head);
-	// printf("count=%d\n", rbuffer->count);
 
 	ringbuffer_insert(rbuffer, 'y');
 	printf("\ninserted %c\n", 'y');
 	ringbuffer_print(rbuffer);
 	printf("tail=%d\n", rbuffer->tail);
 	printf("head=%d\n", rbuffer->head);
-	// printf("count=%d\n", rbuffer->count);
 
 	ringbuffer_insert(rbuffer, '_');
 	printf("\ninserted %c\n", '_');
 	ringbuffer_print(rbuffer);
 	printf("tail=%d\n", rbuffer->tail);
 	printf("head=%d\n", rbuffer->head);
-	// printf("count=%d\n", rbuffer->count);
 	
 	ringbuffer_insert(rbuffer, 'B');
 	printf("\ninserted %c\n", 'B');
 	ringbuffer_print(rbuffer);
 	printf("tail=%d\n", rbuffer->tail);
 	printf("head=%d\n", rbuffer->head);
-	// printf("count=%d\n", rbuffer->count);
 
 	printf("\nremoved %c\n", ringbuffer_remove(rbuffer));
 	ringbuffer_print(rbuffer);
 	printf("tail=%d\n", rbuffer->tail);
 	printf("head=%d\n", rbuffer->head);
-	// printf("count=%d\n", rbuffer->count);
 
 	printf("\nremoved %c\n", ringbuffer_remove(rbuffer));
 	ringbuffer_print(rbuffer);
 	printf("tail=%d\n", rbuffer->tail);
 	printf("head=%d\n", rbuffer->head);
-	// printf("count=%d\n", rbuffer->count);
 
 	ringbuffer_insert(rbuffer, 'a');
 	printf("\ninserted %c\n", 'a');
 	ringbuffer_print(rbuffer);
 	printf("tail=%d\n", rbuffer->tail);
 	printf("head=%d\n", rbuffer->head);
-	// printf("count=%d\n", rbuffer->count);
 	
 	ringbuffer_insert(rbuffer, 'l');
 	printf("\ninserted %c\n", 'l');
 	ringbuffer_print(rbuffer);
 	printf("tail=%d\n", rbuffer->tail);
 	printf("head=%d\n", rbuffer->head);
-	// printf("count=%d\n", rbuffer->count);
 	
+	printf("\nelement at tail is %c\n", ringbuffer_peek(rbuffer));
+	ringbuffer_print(rbuffer);
+	printf("tail=%d\n", rbuffer->tail);
+	printf("head=%d\n", rbuffer->head);
+
+	if (ringbuffer_sfinsert(rbuffer, 'o'))
+		printf("\ninserted %c\n", 'o');
+	else 
+		printf("\ndid not insert... buffer is full\n");
+	ringbuffer_print(rbuffer);
+	printf("tail=%d\n", rbuffer->tail);
+	printf("head=%d\n", rbuffer->head);
+
+	printf("\nelement at tail is %c\n", ringbuffer_peek(rbuffer));
+	ringbuffer_print(rbuffer);
+	printf("tail=%d\n", rbuffer->tail);
+	printf("head=%d\n", rbuffer->head);
+
+	if (ringbuffer_sfinsert(rbuffer, 's'))
+		printf("\ninserted %c\n", 's');
+	else 
+		printf("\ndid not insert... buffer is full\n");
+	ringbuffer_print(rbuffer);
+	printf("tail=%d\n", rbuffer->tail);
+	printf("head=%d\n", rbuffer->head);
+
+	printf("\nremoved %c\n", ringbuffer_remove(rbuffer));
+	ringbuffer_print(rbuffer);
+	printf("tail=%d\n", rbuffer->tail);
+	printf("head=%d\n", rbuffer->head);
+
+	if (ringbuffer_sfinsert(rbuffer, 's'))
+		printf("\ninserted %c\n", 's');
+	else 
+		printf("\ndid not insert... buffer is full\n");
+	ringbuffer_print(rbuffer);
+	printf("tail=%d\n", rbuffer->tail);
+	printf("head=%d\n", rbuffer->head);
+
 	delete_ringbuffer(rbuffer);
 
 	return 0;
